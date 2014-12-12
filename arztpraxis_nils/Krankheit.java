5b80758a13ac6f70fa7d6160f186b1a602d6c90e
Nils001 <xxme1000xx@googlemail.com>
2014-12-12 18:07:01 +0100

    12

---
+++ arztpraxis_nils/Krankheit.java
@@ -0,0 +1,85 @@
+import java.util.Random;
+/**
+ * Write a description of class Krankheit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Krankheit
+{
+    // instance variables - replace the example below with your own
+    private String diagnose;
+    private int Priorität;
+    private Random r;
+    private int a;
+    /**
+     * Constructor for objects of class Krankheit
+     */
+    public Krankheit()
+    {
+        r = new Random();     
+        a = r.nextInt(30);
+        switch(a)
+        {
+            case 0: 
+            this.diagnose = "";
+            this.Priorität =1 ;
+
+            break;
+            case 1: kartenwert = "bauer";
+            break;
+            case 2: kartenwert = "dame";
+            break;
+            case 3: kartenwert = "koenig";
+            break;
+            case 4: kartenwert = "2";
+            break;
+            case 5: kartenwert = "3";
+            break;
+            case 6: kartenwert = "4";
+            break;
+            case 7: kartenwert = "5";
+            break;
+            case 8: kartenwert = "6";
+            break;
+            case 9: kartenwert = "7";
+            break;
+            case 10: kartenwert = "8";
+            break;
+            case 11: kartenwert = "9";
+            break;
+            case 12: kartenwert = "10";
+            break;                 
+            case 0: kartenwert = "ass";
+            break;
+            case 1: kartenwert = "bauer";
+            break;
+            case 2: kartenwert = "dame";
+            break;
+            case 3: kartenwert = "koenig";
+            break;
+            case 4: kartenwert = "2";
+            break;
+            case 5: kartenwert = "3";
+            break;
+            case 6: kartenwert = "4";
+            break;
+            case 7: kartenwert = "5";
+            break;
+            case 8: kartenwert = "6";
+            break;
+            case 9: kartenwert = "7";
+            break;
+            case 10: kartenwert = "8";
+            break;
+            case 11: kartenwert = "9";
+            break;
+            case 12: kartenwert = "10";
+            break;                 
+
+        }
+    }
+
+   
+
+}
