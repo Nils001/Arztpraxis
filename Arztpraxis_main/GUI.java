014a3d0d6ebdbbb504a35d3a6b8da27bbb2eedcd
jonas001 <jonas.youtube@hotmail.de>
2015-01-07 18:29:24 +0100

    gui v2

--- Arztpraxis_main/GUI.java
+++ Arztpraxis_main/GUI.java
@@ -22,7 +22,7 @@ public class GUI extends JFrame implements ActionListener
     public GUI ()
     {
         super("Arztpraxis"); 
-        setSize(500, 500);
+        setSize(600, 600);
         setLayout(null); 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 
@@ -31,62 +31,62 @@ public class GUI extends JFrame implements ActionListener
         add(tAusgabe);
 
         bButton1 = new JButton("Button 1");
-        bButton1.setBounds(280,10, 100, 30);
+        bButton1.setBounds(220,10, 100, 30);
         bButton1.addActionListener(this); 
         add(bButton1);
 
         bButton2 = new JButton("Button 2");
-        bButton2.setBounds(280,10, 100, 30);
+        bButton2.setBounds(220,50, 100, 30);
         bButton2.addActionListener(this); 
-        add(bButton1);
+        add(bButton2);
 
         bButton3 = new JButton("Button 3");
-        bButton3.setBounds(280,10, 100, 30);
+        bButton3.setBounds(220,90, 100, 30);
         bButton3.addActionListener(this); 
         add(bButton3);
 
         bButton4 = new JButton("Button 4");
-        bButton4.setBounds(280,10, 100, 30);
+        bButton4.setBounds(220,130, 100, 30);
         bButton4.addActionListener(this); 
         add(bButton4);
 
         bButton5 = new JButton("Button 5");
-        bButton5.setBounds(280,10, 100, 30);
+        bButton5.setBounds(220,170, 100, 30);
         bButton5.addActionListener(this); 
         add(bButton5);
 
         bButton6 = new JButton("Button 6");
-        bButton6.setBounds(280,10, 100, 30);
+        bButton6.setBounds(220,210, 100, 30);
         bButton6.addActionListener(this); 
         add(bButton6);
 
         bButton7 = new JButton("Button 7");
-        bButton7.setBounds(280,10, 100, 30);
+        bButton7.setBounds(220,250, 100, 30);
         bButton7.addActionListener(this); 
         add(bButton7);
 
         bButton8 = new JButton("Button 8");
-        bButton8.setBounds(280,10, 100, 30);
+        bButton8.setBounds(220,290, 100, 30);
         bButton8.addActionListener(this); 
         add(bButton8);
 
         bButton9 = new JButton("Button 9");
-        bButton9.setBounds(280,10, 100, 30);
+        bButton9.setBounds(220,330, 100, 30);
         bButton9.addActionListener(this); 
         add(bButton9);
 
         bButton10 = new JButton("Button 10");
-        bButton10.setBounds(280,10, 100, 30);
+        bButton10.setBounds(220,370, 100, 30);
         bButton10.addActionListener(this); 
         add(bButton10);
 
         bButton11 = new JButton("Button 11");
-        bButton11.setBounds(280,10, 100, 30);
+        bButton11.setBounds(220,410, 100, 30);
         bButton11.addActionListener(this); 
         add(bButton11);
 
         bButton12 = new JButton("Button 12");
-        bButton12.setBounds(280,10, 100, 30);
+        bButton12.setBounds(220,450, 100, 30);
         bButton12.addActionListener(this); 
         add(bButton12);
 
